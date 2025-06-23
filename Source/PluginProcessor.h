d754cdde37ffac908b4d8101a7c073ffa730cdec
misepe <msegper@teleco.upv.es>
2025-06-23 18:15:52 +0200

    connected the low cut parameters to the filter

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -10,11 +10,18 @@
 
 #include <JuceHeader.h>
 
+enum Slope {
+	Slope_12,
+	Slope_24,
+	Slope_36,
+	Slope_48
+};
+
 struct ChainSettings
 {
 	float peakFreq{ 0 }, peakGainInDecibels{ 0 }, peakQuality{ 1.f };
 	float lowCutFreq{ 0 }, highCutFreq{ 0 };
-	int lowCutSlope{ 0 }, highCutSlope{ 0 };
+	int lowCutSlope{ Slope::Slope_12 }, highCutSlope{ Slope::Slope_12 };
 };
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts);
@@ -68,7 +75,7 @@ public:
 private:
     using Filter = juce::dsp::IIR::Filter<float>;
 
-	using CutFilter = juce::dsp::ProcessorChain <Filter, Filter, Filter>;
+	using CutFilter = juce::dsp::ProcessorChain <Filter, Filter, Filter, Filter>;
 
 	using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
 
