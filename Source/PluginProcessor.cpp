561617618e9b97450d3519c6ffc1eeec82e430e3
misepe <msegper@teleco.upv.es>
2025-06-24 17:15:21 +0200

    edit sliders of the gui and conected to param

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -183,8 +183,8 @@ bool Project_EEAVAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* Project_EEAVAudioProcessor::createEditor()
 {
-    //return new Project_EEAVAudioProcessorEditor (*this);
-    return new juce::GenericAudioProcessorEditor(*this);
+    return new Project_EEAVAudioProcessorEditor (*this);
+    //return new juce::GenericAudioProcessorEditor(*this);
 }
 
 //==============================================================================
@@ -193,12 +193,21 @@ void Project_EEAVAudioProcessor::getStateInformation (juce::MemoryBlock& destDat
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
+
+    juce::MemoryOutputStream mos (destData, true);
+	apvts.state.writeToStream(mos);
 }
 
 void Project_EEAVAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
+	auto tree = juce::ValueTree::readFromData(data, sizeInBytes);
+    if (tree.isValid())
+    {
+		apvts.replaceState(tree);
+		updateFilters();
+    }
 }
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts) 
