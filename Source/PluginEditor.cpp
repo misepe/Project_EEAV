31c3abf6ad52c996c3d653d907fa6b3ad05389b4
misepe <msegper@teleco.upv.es>
2025-06-24 18:23:06 +0200

    response curve now shows cut filter response

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -157,6 +157,11 @@ void Project_EEAVAudioProcessorEditor::timerCallback()
 		auto chainSettings = getChainSettings(audioProcessor.apvts);
 		auto peakCoefficients = makePeakFilter(chainSettings, audioProcessor.getSampleRate());
 		updateCoefficients(monoChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
+
+		auto lowCutCoefficients = makeLowCutFilter(chainSettings, audioProcessor.getSampleRate());
+		auto highCutCoefficients = makeHighCutFilter(chainSettings, audioProcessor.getSampleRate());
+		updateCutFilter(monoChain.get<ChainPositions::LowCut>(), lowCutCoefficients, static_cast<Slope>(chainSettings.lowCutSlope));
+		updateCutFilter(monoChain.get<ChainPositions::HighCut>(), highCutCoefficients, static_cast<Slope>(chainSettings.highCutSlope));
         //signal a repaint
         repaint();
 	}
