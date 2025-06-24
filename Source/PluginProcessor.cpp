ec12ad9385e8b6e9f9826f33ed3dbdaadde684bd
misepe <msegper@teleco.upv.es>
2025-06-24 18:07:30 +0200

    draw response curve and refactored Peak coefficient generation

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -226,19 +226,23 @@ ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts)
 	return settings;
 }
 
+Coefficients makePeakFilter(const ChainSettings& chainSettings, double sampleRate)
+{
+	return juce::dsp::IIR::Coefficients<float>::makePeakFilter(sampleRate,
+		chainSettings.peakFreq,
+		chainSettings.peakQuality,
+		juce::Decibels::decibelsToGain(chainSettings.peakGainInDecibels));
+}
+
 void Project_EEAVAudioProcessor::updatePeakFilter(const ChainSettings &chainSettings)
 {
-
-    auto peakCoefficients = juce::dsp::IIR::Coefficients<float>::makePeakFilter(getSampleRate(),
-        chainSettings.peakFreq,
-        chainSettings.peakQuality,
-        juce::Decibels::decibelsToGain(chainSettings.peakGainInDecibels));
+	auto peakCoefficients = makePeakFilter(chainSettings, getSampleRate());
 
 	updateCoefficients(leftChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
     updateCoefficients(rightChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
 }
 
-void Project_EEAVAudioProcessor::updateCoefficients(Coefficients &old, const Coefficients &replacements) 
+void updateCoefficients(Coefficients & old, const Coefficients & replacements)
 {
 	*old = *replacements;
 }
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -26,6 +26,24 @@ struct ChainSettings
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts);
 
+using Filter = juce::dsp::IIR::Filter<float>;
+
+using CutFilter = juce::dsp::ProcessorChain <Filter, Filter, Filter, Filter>;
+
+using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
+
+enum ChainPositions
+{
+    LowCut,
+    Peak,
+    HighCut
+};
+
+using Coefficients = Filter::CoefficientsPtr;
+void updateCoefficients(Coefficients& old, const Coefficients& replacement);
+
+Coefficients makePeakFilter(const ChainSettings& chainSettings, double sampleRate);
+
 //==============================================================================
 /**
 */
@@ -73,24 +91,10 @@ public:
     juce::AudioProcessorValueTreeState apvts{ *this,nullptr,"Parameters",createParameterLayout()};
 
 private:
-    using Filter = juce::dsp::IIR::Filter<float>;
-
-	using CutFilter = juce::dsp::ProcessorChain <Filter, Filter, Filter, Filter>;
-
-	using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
-
 	MonoChain leftChain, rightChain;
 
-	enum ChainPositions
-	{
-		LowCut,
-		Peak,
-		HighCut
-	};
-
+	
     void updatePeakFilter(const ChainSettings &chainSettings);
-	using Coefficients = Filter::CoefficientsPtr;
-    static void updateCoefficients(Coefficients& old, const Coefficients& replacement);
 
     template<int Index, typename ChainType, typename CoefficientType>
     void update(ChainType& chain, const CoefficientType& coefficients)
